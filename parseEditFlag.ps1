8e3f7a4eba791f23e40ef2c2a07233afd1ab44c8
Jake Hildreth <jakehildreth@gmail.com>
2025-05-05 20:46:06 -0400

    Now collecting EditFlags. MVP complete!

---
+++ parseEditFlag.ps1
@@ -0,0 +1,25 @@
+function parseEditFlag {
+    param (
+        $EditFlag
+    )
+
+    [array]$EditFlagCollection = $EditFlag | ForEach-Object {
+        $Flag = ($_.trim().split(' -- '))[0] | Select-String 'EDITF_'
+        Write-Host $_
+        if ($null -ne $Flag) {
+            if ($Flag -match '^\(EDITF_') {
+                $Flag = $Flag.ToString().Substring(1)
+                $Enabled = $false
+            } else {
+                $Enabled = $true
+            }
+
+            [PSCustomObject]@{
+                EditFlag = $Flag
+                Enabled  = $Enabled
+            }
+        }
+    }
+
+    $EditFlagCollection
+}
