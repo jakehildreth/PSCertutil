8d6337e55f82a0e80e472c9d557bde397f5b6606
Jake Hildreth <jakehildreth@gmail.com>
2025-07-28 21:09:19 -0400

    chore: added TODO to parseModifyFlag.ps1

--- parseModifyFlag.ps1
+++ parseModifyFlag.ps1
@@ -5,6 +5,7 @@ function parseModifyFlag {
 
     if ($ModifyFlag -notcontains 'CertUtil: -setreg command completed successfully.') {
         $exception = $ModifyFlag -join ', '
+        # TODO Start returning more specific error messages.
         [System.Management.Automation.ErrorRecord]::new($exception, 'CustomStringError', 'NotSpecified', $null)
     }
 }
